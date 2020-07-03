ed721a83fc8df0a53dc4cae647b2bcfa926f7ddb
Gary Bergman <garybergman00@gmail.com>
2020-07-03 17:43:55 -0400

    Added static to express server and get api/notes

--- expressServer.js
+++ expressServer.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const server = express();
 const path = require("path");
+const { static } = require("express");
 const PORT = 8080;
 
 // use, get, post, delete
+// need to fs.writefile to db.json
 
 server.listen((PORT), () => {
     console.log("Server has started listening on port: " + PORT);
 })
+
+// Static allows the server to make connections between the files in the folder and sub-folders of "public" in relation to the gets we've accessed
+server.use(express.static("public"))
 
 
 server.get("/", (req, res) => {
@@ -23,3 +28,6 @@ server.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/notes.html"));
 })
 
+server.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname + "/db/db.json"));
+})
