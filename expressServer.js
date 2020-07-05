e14dc32a2fad9701378f0774cd6d71e219fde0ae
Gary Bergman <garybergman00@gmail.com>
2020-07-05 13:26:26 -0400

    Wrote starter code for adding and deleting notes to and from db.json but still need to incorperate fs

--- expressServer.js
+++ expressServer.js
@@ -2,10 +2,12 @@ const express = require("express");
 const server = express();
 const path = require("path");
 const { static } = require("express");
+const { fstat } = require("fs");
 const PORT = 8080;
 
 // use, get, post, delete
 // need to fs.writefile to db.json
+
 
 server.listen((PORT), () => {
     console.log("Server has started listening on port: " + PORT);
@@ -14,7 +16,7 @@ server.listen((PORT), () => {
 // Static allows the server to make connections between the files in the folder and sub-folders of "public" in relation to the gets we've accessed
 server.use(express.static("public"))
 
-
+// Get index page
 server.get("/", (req, res) => {
     // res.send("Welcom to my page.");
     // if (broken){
@@ -24,10 +26,25 @@ server.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/index.html"));
 })
 
+
+// Get Notes Page
 server.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/notes.html"));
 })
 
+
+// Get notes from db 
 server.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname + "/db/db.json"));
 })
+
+// Post notes to db
+server.post("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname + "/db/db.json"));
+})
+
+// Delete notes from db
+server.delete("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname + "/db/db.json"));
+})
+
